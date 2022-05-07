370d81c487fa2e8c9be28dfda23d869cb782f894
Diabolik90 <davidepie90@gmail.com>
2022-05-07 13:51:38 +0200

    Second exercise

---
+++ Ejercicio_tema_2/index.js
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio tema 2
+ * 
+ * Enunciado del ejercicio: 
+ * En este ejercicio tienes que crear un archivo JavaScript donde crees una variable la cual contenga una cadena de texto.
+ * Después crearás una función, dentro de ella crearas una variable que se llame igual que la primera que has creado y le asignaras 
+ * una cadena de texto diferente a la primera y mostrarás la variable por consola.
+ * Por último, fuera de la función tendrás que realizar la llamada a la función y mostrarás por consola la primera variable que has creado.
+ */
+
+function nuevo_texto(){
+    let str = 'Hola Open-BootCamp'
+    console.log(str)
+}
+
+
+let str = "Hola mundo"
+console.log(str)
+
+nuevo_texto()
+
+console.log(str)
+
