c80a3d9078d0b0d1feb05f4b2dfc9d4d8b8e2370
Diabolik90 <davidepie90@gmail.com>
2022-05-07 19:18:06 +0200

    Fourth exercise

---
+++ Ejercicio_tema_4/ejercicio_1_tema_4.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 1 tema 4
+ * 
+ * Enunciado del ejercicio: 
+ * En este primer ejercicio tienes que crear una cadena de texto con una frase, por ejemplo: 
+ * “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. 
+ * Por consola tienes que mostrar la frase con las palabras cambiadas.
+ */
+
+let str = 'El perro de mi vecina es muy molesto'
+console.log(str)
+console.log(str.replace('vecina','vecino'))
---
+++ Ejercicio_tema_4/ejercicio_2_tema_4.js
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 2 tema 4
+ * 
+ * Enunciado del ejercicio: 
+ * Como segundo ejercicio, tienes que crear otra variable con una cadena de texto 
+ * y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.
+ */
+
+let str = '!Hola mundo de Open-BootCamp!'
+console.log(str);
+console.log(str.toUpperCase())
