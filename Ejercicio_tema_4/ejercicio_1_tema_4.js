f84603ef0ec33bde6e8eac57d07b420083fc4498
Diabolik90 <davidepie90@gmail.com>
2022-05-10 15:18:26 +0200

    edited 4th exercise

--- Ejercicio_tema_4/ejercicio_1_tema_4.js
+++ Ejercicio_tema_4/ejercicio_1_tema_4.js
@@ -2,11 +2,42 @@
  * Ejercicio 1 tema 4
  * 
  * Enunciado del ejercicio: 
- * En este primer ejercicio tienes que crear una cadena de texto con una frase, por ejemplo: 
- * “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. 
- * Por consola tienes que mostrar la frase con las palabras cambiadas.
+ * Crea un archivo JS que contenga las siguientes líneas
+ * - Una cadena de texto con tu Nombre
+ * - Otra cadena de texto con tu Apellido
+ * - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+ * - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+ * - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+ * - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+ * - Una variable que contenga la primera letra del Nombre
+ * - Otra variable que contenga la última letra del Apellido
+ * - Una cadena de texto que elimine los espacios de la variable "estudiante"
+ * - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
  */
 
-let str = 'El perro de mi vecina es muy molesto'
-console.log(str)
-console.log(str.replace('vecina','vecino'))
+var nombre = 'Davide'
+var apellido = 'Fragnito'
+
+var estudiante = nombre.concat(' ', apellido)
+console.log(estudiante)
+
+var estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+
+var estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+
+var size = estudiante.length
+console.log(size)
+
+var primeraLetra = nombre.charAt(0)
+console.log(primeraLetra)
+
+var ultimaLetra = apellido.charAt(apellido.length - 1)
+console.log(ultimaLetra)
+
+var cadenaSinEspacios = estudiante.replace(/ /g, '')
+console.log(cadenaSinEspacios)
+
+var contiene = estudiante.includes("Davide")
+console.log(contiene)
